e8215650f4161dcce34b0329f82916fb478e288c
Olli Sulopuisto <dst@iki.fi>
2026-05-29 22:40:29 +0300

    feat(kissagotchi): rewrite in Rust and Macroquad WASM

--- games_repo/games/bubbles/src/main.rs
+++ games_repo/games/bubbles/src/main.rs
@@ -11,7 +11,7 @@ use crate::input::InputManager;
 use crate::game::{Game, VIRTUAL_WIDTH, HUD_HEIGHT, PLAY_HEIGHT};
 
 #[allow(dead_code)]
-const VERSION: &str = "26.05.29.262";
+const VERSION: &str = "26.05.29.263";
 
 #[derive(Clone, PartialEq, Debug)]
 enum AppState {
